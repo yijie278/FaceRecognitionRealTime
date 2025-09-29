d634d8e836a08da994d7f3f261a3f50004f30113
yijie <limyijie402@gmail.com>
2025-09-29 22:23:39 +0800

    testing face matching, compare current face from webcam and image in folder

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pickle
+import face_recognition
 import cv2
 import os
 
@@ -15,11 +17,39 @@ for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 #print(len(imgModeList))
 
+#Load the encoding file
+print("Loading Encode file..")
+file =open('EncodeFile.p', 'rb') #read file
+encodeListUnknownWithIds = pickle.load(file) # add all list and info into encodeListUnknownWithIds
+file.close()
+encodeListKnown, studentIds= encodeListUnknownWithIds
+print("Encode File Loaded")
+
+
 
 while True:
     success, img = cap.read()
+
+    imgS=cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    #compare two face one from camera, one from encoding
+    faceCurFrame=face_recognition.face_locations(imgS)
+    encodeCurFrame=face_recognition.face_encodings(imgS,faceCurFrame)
+
+
     imgBackground[162:162+480,55:55+640]=img
     imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
+
+    # use zip so no need separate in two loop, encodeFace is current face
+    # the lower distance , the better match
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis=face_recognition.face_distance(encodeListKnown, encodeFace)
+        print("matches",matches)
+        print("faceDis",faceDis)
+
+
 
     cv2.imshow("Webcam", img)
     cv2.imshow('Face Attendance', imgBackground)
