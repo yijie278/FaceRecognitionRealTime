ae626a887eade2ccba5a42cfd33324f8e3b37302
yijie <limyijie402@gmail.com>
2025-09-29 21:37:51 +0800

    import image func

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -9,4 +9,4 @@ modePathList = os.listdir(folderPath)
 imgList = []
 for path in modePathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
-print(len(imgList))
+#print(len(imgList))
